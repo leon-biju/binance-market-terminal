539059c38ed3cdd4c3a0918143a2cea90dff6057
Leon Biju <31255961+leon-biju@users.noreply.github.com>
2025-12-03 19:57:23 +0000

    add binance fetching depth snapshots

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+mod binance;
+mod snapshot;
+
+use anyhow::Result;
+
+
+#[tokio::main]
+async fn main() -> Result<()>{
     println!("Hello, world!");
+    
+    let snapshot = snapshot::fetch_snapshot("BTCUSDT", 1).await?;
+    println!("{:?}", snapshot);
+    Ok(())
 }
